f08522a8ffd7cf8c2379bef9ba6b8963a24a4ecf
Brian Fedelin <brianfedelin1@gmail.com>
2021-05-27 14:37:56 -0700

    Initial Commit

---
+++ FRUKTS/FRUKTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRUKTSApp.swift
+//  FRUKTS
+//
+//  Created by Brian Fedelin on 5/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FRUKTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
